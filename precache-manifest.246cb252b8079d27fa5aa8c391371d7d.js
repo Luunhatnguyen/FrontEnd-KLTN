8a28d94cf34798a5bf965f27b8b1d401050c83dd
Luunhatnguyen <nhatnguyen.01102001@gmail.com>
2023-05-17 11:03:48 +0700

    Updates

---
+++ precache-manifest.246cb252b8079d27fa5aa8c391371d7d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6ae9b61a1f0aac1344",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/css/main.5da7779b.chunk.css"
+  },
+  {
+    "revision": "ff6ae9b61a1f0aac1344",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/js/main.ff6ae9b6.chunk.js"
+  },
+  {
+    "revision": "112fea1bc9f7b6973477",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/js/1.112fea1b.chunk.js"
+  },
+  {
+    "revision": "92cbdfa5dfacec452d0a",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/css/2.07730448.chunk.css"
+  },
+  {
+    "revision": "92cbdfa5dfacec452d0a",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/js/2.92cbdfa5.chunk.js"
+  },
+  {
+    "revision": "29d1d669cdeb3aa0be26",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/js/runtime~main.29d1d669.js"
+  },
+  {
+    "revision": "41f63af8a652d258be084d91e7698568",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/14926f75f7d51ac044ccc0847cfb262f.41f63af8.png"
+  },
+  {
+    "revision": "25b989592ff01bcbb1edadb924e8b335",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/quynhon.25b98959.jpg"
+  },
+  {
+    "revision": "447b6b2722b599c5e224790fe2c2c6bc",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/advice-1.447b6b27.jpg"
+  },
+  {
+    "revision": "81590755ddb87163edf9ccf4ba153ca9",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/Bus-Station-High-Quality-Wallpaper.81590755.jpg"
+  },
+  {
+    "revision": "21c52e44fe2774424b4067ae94058d32",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/page-title-5.21c52e44.jpg"
+  },
+  {
+    "revision": "908c9c347521ee99ea099fb6b07c9f61",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/page-title-9.908c9c34.jpg"
+  },
+  {
+    "revision": "30ab8a66f6a8ed852a437efc5dc1a274",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/yokohama-bus-1f-1280x800.30ab8a66.jpg"
+  },
+  {
+    "revision": "fc68813e3f0b435b75ec0d014fb9a5f0",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/hallstatt-g22b586807_1920.fc68813e.jpg"
+  },
+  {
+    "revision": "1c8aa38e405351faa75438f57094dd85",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/bus-station.1c8aa38e.webp"
+  },
+  {
+    "revision": "2a0f23665fa394da849fd787284e36a2",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/shape-2.2a0f2366.png"
+  },
+  {
+    "revision": "5a6cc75a643a95e4e1b91275ffb3c92c",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/shape-13.5a6cc75a.png"
+  },
+  {
+    "revision": "26ce57dd1130997be7934eb7b8932ca3",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/banner.26ce57dd.jpg"
+  },
+  {
+    "revision": "7ed1b7886fa16788e0ca4ecf2ef9b81b",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/video-1.7ed1b788.jpg"
+  },
+  {
+    "revision": "d9af351b0af5a226427aa83927d393b8",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/istockphoto-485916538-1024x1024.d9af351b.jpg"
+  },
+  {
+    "revision": "6d08f65bf8132eac047a414a1095a807",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/logo-zalopay.6d08f65b.svg"
+  },
+  {
+    "revision": "f6f7ec72a6b75af94de64ad3aa5b02c8",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/5282.f6f7ec72.jpg"
+  },
+  {
+    "revision": "b27cb3c88f015c2e6d0090050da000c2",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/static/media/page-title.b27cb3c8.jpg"
+  },
+  {
+    "revision": "a3d1d1a7fc174df97843e42b28397635",
+    "url": "/Luunhatnguyen/FrontEnd-KLTN/index.html"
+  }
+];
